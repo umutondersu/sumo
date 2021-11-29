93dbf41925f65de452f6566b5d3eb84988591169
CodedTheBear <inan.berk.business@gmail.com>
2021-11-29 03:27:52 +0300

    Calculation Page FrontEnd Design Update

---
+++ sumo_frontend/src/Pages/Calculation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import Avatar from 'react-avatar';
+import './Summary.css'
+import axios from 'axios';
+import Header from '../Components/General/Header';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import CloseIcon from '@mui/icons-material/Close';
+import Alert from '@mui/material/Alert';
+import Chart from "react-google-charts";
+
+
+function Calculation () {
+
+
+
+
+}
+
+export default Calculation
