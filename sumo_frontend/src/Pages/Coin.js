c3f1c9f1c7f82f779ae57926cf2058ac1b62387e
CodedTheBear <inan.berk.business@gmail.com>
2021-12-13 15:45:58 +0300

    STOCK_CHANGES

--- sumo_frontend/src/Pages/Coin.js
+++ sumo_frontend/src/Pages/Coin.js
@@ -19,17 +19,17 @@ const Coin = ({
           <p className='coin-symbol'>{symbol}</p>
         </div>
         <div className='coin-data'>
-          <p className='coin-price'>${price}</p>
-          <p className='coin-volume'>${volume.toLocaleString()}</p>
+          <p className='coin-price'>{price} TL</p>
+          <p className='coin-volume'>{volume.toLocaleString()} TL</p>
 
           {priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent red'> {priceChange.toFixed(2)}%</p>
           ) : (
             <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
           )}
 
           <p className='coin-marketcap'>
-            Mkt Cap: ${marketcap.toLocaleString()}
+            Mkt Cap: {marketcap.toLocaleString()} TL
           </p>
         </div>
       </div>
