e9e5e07037342ececf7194f3d6a22585a5583de2
CodedTheBear <inan.berk.business@gmail.com>
2021-12-17 00:43:26 +0300

    Exchange Page

--- sumo_frontend/src/App.js
+++ sumo_frontend/src/App.js
@@ -13,6 +13,7 @@ import Stock from './Pages/Stock';
 import axios from 'axios';
 import ExpertLogin from './Pages/ExpertLogin';
 import Expert from './Pages/Expert';
+import Currency from './Pages/Currency';
 
 function App() {
   
@@ -33,6 +34,7 @@ function App() {
       <Route path="/Calculation" component={Calculation} exact/>
       <Route path="/Advisor" component={Advisor} exact/>
       <Route path="/Stock" component={Stock} exact/>
+      <Route path="/Currency" component={Currency} exact/>
     </BrowserRouter>
   );
 }
