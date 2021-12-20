d51eab95c40b35d4994ddd8db36baaaeeeaeb81a
atasarikaya <atasarikaya@sabanciuniv.edu>
2021-12-20 14:40:48 +0300

    CurrencyPageChanges

--- sumo_frontend/src/Components/General/Coin.js
+++ sumo_frontend/src/Components/General/Coin.js
@@ -19,18 +19,28 @@ const Coin = ({
           <p className='coin-symbol'>{symbol}</p>
         </div>
         <div className='coin-data'>
-          <p className='coin-price'>{price} TL</p>
-          <p className='coin-volume'>{volume.toLocaleString()} TL</p>
+            <div className="priceInfo">
+                <p>Price:</p>
+                <p className='coin-price'>{price} ₺</p>
+            </div>
+            <div className="priceInfo">
+                <p>Volume:</p>
+                <p className='coin-volume'>{volume.toLocaleString()} ₺</p>
+            </div>
 
-          {priceChange < 0 ? (
-            <p className='coin-percent red'> {priceChange.toFixed(2)}%</p>
-          ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
-          )}
+            <div className="priceInfo">
+                <p>Change:</p>
+                {priceChange < 0 ? (
+                <p className='coin-percent red'> {priceChange.toFixed(2)}%</p>
+                ) : (
+                    <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+                )}
+            </div>
 
-          <p className='coin-marketcap'>
-            Mkt Cap: {marketcap.toLocaleString()} TL
-          </p>
+            <div className="priceInfo">
+                <p>Mkt Cap:</p>
+                <p className='coin-volume'>{marketcap.toLocaleString()} ₺</p>
+            </div>
         </div>
       </div>
     </div>
